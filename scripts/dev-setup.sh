730e2c3fe212827b10504d3c5d4d46e450850db0
willdavsmith <willdavsmith@gmail.com>
2025-10-29 11:31:36 -0700

    WIP
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -12,4 +12,7 @@ sleep 5
 rad env create dev --group default
 rad workspace create kubernetes radius-dev --context kind-radius-dev --group default --environment dev --force
 
+rad resource-type create postgreSqlDatabases -f ./radius/data/postgresql/types.yaml
+rad recipe register default --environment dev --resource-type Radius.Data/postgreSqlDatabases --template-kind bicep --template-path ghcr.io/willdavsmith/radius/recipes/postgresql:2.0.0
+
 echo "Development environment setup complete."
